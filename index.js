67cca91ebe475ffc629008db2258996dcae07abd
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-06-30 13:20:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+// Write your solution in this file
+    var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }leastFavoriteCustomer
+
+  const leastFavoriteCustomer = 'someone';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new least favorite';
+    
+  }
+  console.log(customerName);
+  console.log(customerName);
+  upperCaseCustomerName();
+
+  console.log(bestCustomer);
+  setBestCustomer();
+  console.log(bestCustomer);
+overwriteBestCustomer();
+console.log(bestCustomer);
+
+console.log(leastFavoriteCustomer);
+changeLeastFavoriteCustomer();
